cae543ec3a1af3fe88996d023fd6c1fb0e6c407b
Odongo006 <www.henryodongo006@gmail.com>
2023-05-04 18:29:07 +0300

    Made required changes by reviewer

--- queries.sql
+++ queries.sql
@@ -1,3 +1,8 @@
+/*Queries that provide answers to the questions from all projects.*/
+
+SELECT * from animals WHERE name = 'Luna';
+/* Project 1 table animals queries */
+
 SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
